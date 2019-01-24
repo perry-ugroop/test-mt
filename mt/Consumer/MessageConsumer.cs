2db1ab70b3d0af9f16c9fd1d397bd558a19bc6ec
Perry Valdez <perry.valdez@ugroop.com>
2019-01-24 11:33:27 +0800

    Simplified syntax

--- mt/Consumer/MessageConsumer.cs
+++ mt/Consumer/MessageConsumer.cs
@@ -12,11 +12,7 @@ namespace Mt.Consumer
         {
             var sb = new StringBuilder();
 
-            sb.AppendLine(String.Format("Got the following message (#{0}) from {1}:",
-                context.Message.Id,
-                context.Message.Author
-              ));
-
+            sb.AppendLine($"Got the following message (#{context.Message.Id}) from {context.Message.Author}:");
             sb.AppendLine(context.Message.Text);
             sb.AppendLine("");
             
